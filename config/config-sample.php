601af589263f2c8fc2e9a930b07b7a87336c1f7c
ayman-benammour <benammourayman@gmail.com>
2023-10-16 12:54:15 +0200

    :sparkles: ticket-0001 - fix/login - Ajout des balises 'name' au formulaire de Login. #1
    Signed-off-by: ayman-benammour <benammourayman@gmail.com>

--- config/config-sample.php
+++ config/config-sample.php
@@ -6,11 +6,11 @@ $user = 'username';
 $pass = 'password';
 
 
-try{
-	$db = new PDO('mysql:host='.$host.';dbname='.$dbname, $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
-}
-catch(Exception $e)
-{
-	echo 'Erreur : '.$e->getMessage().'<br />';
-	echo 'N° : '.$e->getCode();
-}
+// try{
+// 	$db = new PDO('mysql:host='.$host.';dbname='.$dbname, $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+// }
+// catch(Exception $e)
+// {
+// 	echo 'Erreur : '.$e->getMessage().'<br />';
+// 	echo 'N° : '.$e->getCode();
+// }
